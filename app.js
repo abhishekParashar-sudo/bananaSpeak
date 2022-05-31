dd1d3173bf44823b27a53ef67027152be670c92f
Abhishek Parashar <52458867+abhishekParashar-sudo@users.noreply.github.com>
2022-05-31 17:40:06 +0530

    translate

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var translateBtn = document.querySelector("#translateBtn")
 var txtInput = document.querySelector("#textArea")
 var output = document.querySelector("#textArea2")
 
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-function EventHandler(){
-output.innerText="it is " + txtInput.value
-
+function translateURL(text){
+return url+"?"+"text:"+text
 }
+function EventHandler(){
+var inputText=txtInput.value
+fetch(translateURL(inputText))
+.then(response=>response.json())
+.then(json=>console.log(json))
+};
 
 translateBtn.addEventListener("click",EventHandler)
 
